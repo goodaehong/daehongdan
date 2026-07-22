ad0655037d97df2da2c4b7699607e888c6b5e8a8
gromitKim26 <dudhkd12dbsk@gmail.com>
2026-07-22 12:49:04 +0900

    chore: 중복 ADS1115 초기 스켈레톤 제거

--- drivers/gas_sensor/ads1115_driver.c
+++
@@ -1,7 +0,0 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/i2c.h>
-#include <linux/of_device.h>
-#include <linux/mutex.h>
-#include <linux/delay.h>
-#include <linux/byteorder/generic.h>  // swab16
